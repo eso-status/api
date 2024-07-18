27a136a084b1c3b6551ef217e19c6043b2e05c4b
dov118 <contact@dov118.dev>
2024-07-18 12:47:12 +0200

    Add slug entity

--- src/db/seeders/slug.seeder.ts
+++ src/db/seeders/slug.seeder.ts
@@ -5,6 +5,7 @@ import { Slug } from '../../resource/slug/entities/slug.entity';
 export class SlugSeeder implements Seeder {
   public async run(
     dataSource: DataSource,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     dataSource.getRepository(Slug);
